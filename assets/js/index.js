72d4f47521b3c8cee601fea71baf713565bf8012
Athersh-SR <athersh946@gmail.com>
2025-05-08 09:46:56 +0530

    third  commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,20 @@ passIcon.addEventListener('click', () => {
     passIcon.classList.toggle('fa-eye-slash');
     passIcon.classList.toggle('fa-eye');
 });
+  const templateBtn = document.getElementById('templateBtn');
+  const templateOptions = document.getElementById('templateOptions');
+  const stylesheetLink = document.querySelector('link[href*="template_"]');
+
+  templateBtn.addEventListener('click', () => {
+    templateOptions.classList.toggle('d-none');
+  });
+
+  templateOptions.addEventListener('click', (e) => {
+    if (e.target.matches('a[data-template]')) {
+      e.preventDefault();
+      const newTemplate = e.target.getAttribute('data-template');
+      stylesheetLink.href = `./assets/css/${newTemplate}`;
+      templateOptions.classList.add('d-none');
+    }
+  });
+
